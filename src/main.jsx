271dfb87d1e3cab1ed0f2ebd7c2638c0c7ba0a6f
bhattarayudh.p <bhattarayudh.p@kbtg.tech>
2022-04-28 16:58:10 +0700

    use global context

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App'
-import Workshop from './workshop/Workshop'
+import DemoProps from './workshop/WorkProps'
 // import AppClass from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Workshop />
+  <DemoProps />
     {/* <AppClass /> */}
   </React.StrictMode>
 )
