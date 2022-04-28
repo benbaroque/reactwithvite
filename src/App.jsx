d822399582f19a6e3a3eda9d6d15747bdfbedf1a
bhattarayudh.p <bhattarayudh.p@kbtg.tech>
2022-04-28 12:06:32 +0700

    Step 2 Convert to Component Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { Component } from 'react'
 import './App.css'
 
 function App() {
@@ -40,4 +39,35 @@ function App() {
   )
 }
 
-export default App
+class AppClass extends Component {
+  message = "Hello Class Component";
+
+  constructor() {
+    super();
+    this.state = {
+      count: 10,
+    };
+  }
+
+  setCount(newValue) {
+    this.setState({ count: newValue });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{this.message}</p>
+          <p>
+            <button type="button"
+              onClick={() => this.setCount(this.state.count + 1)}>
+              count is: {this.state.count}
+            </button>
+          </p>
+        </header>
+      </div>
+    )
+  };
+}
+
+export default AppClass
