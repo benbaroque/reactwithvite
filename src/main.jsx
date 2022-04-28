d53d0a65dc28c0dd8ae90159924880afb94a7282
bhattarayudh.p <bhattarayudh.p@kbtg.tech>
2022-04-28 14:55:26 +0700

    step 5 init component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
+import Workshop from './workshop/Workshop'
 // import AppClass from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <App />
+  <Workshop />
     {/* <AppClass /> */}
   </React.StrictMode>
 )
---
+++ src/workshop/Hello.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+const Hello = () => {
+    const [name, updateName] = useState("")
+    return (
+        <div>
+            <form>
+                Name : <input type="text" />
+            </form>
+            {name ? <b>Hi, {name}</b> : "Please enter your name"}
+        </div>
+    )
+}
+
+export default Hello 
